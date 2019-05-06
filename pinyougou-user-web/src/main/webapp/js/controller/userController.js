ae44e5a8cf4dce3622cc6c3f6d987eddf849ba70
laozhongyuan <laozhongyuan@users.noreply.github.com>
2019-05-07 07:31:47 +0800

    获取用户登录名

--- pinyougou-user-web/src/main/webapp/js/controller/userController.js
+++ pinyougou-user-web/src/main/webapp/js/controller/userController.js
@@ -1,5 +1,46 @@
  //控制层 
-app.controller('userController' ,function($scope,userService){
+app.controller('userController' ,function($scope,userService,findOrdersService,loginService){
+
+//获取用户登录信息
+	$scope.showName = function () {
+		loginService.loadLoginName().success(function (data) {
+			$scope.loginName = data.loginName;
+			$scope.loginPic = data.loginPic;
+			$scope.findOne($scope.loginName);
+			$scope.findAddressList();
+		})
+	};
+
+
+
+	//控制层
+	app.controller('findOrdersController' ,function($scope,findOrdersService) {
+
+		$scope.findAllOrders=function(){
+			findOrdersService.findAllOrders().success(
+				function (result) {
+					$scope.Orderlist=result;
+				}
+			)
+		}
+
+		$scope.topayhtml=function (orderId) {
+			window.location="/pay.html#?orderId="+orderId;
+		}
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	//写一个方法  当点击注册的时候调用 发送请求 将数据存储到数据库
 
@@ -30,5 +71,5 @@ app.controller('userController' ,function($scope,userService){
 		)
     }
 
-    
+
 });	
